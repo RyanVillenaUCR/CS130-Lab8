191ee061f7b6a03f91d4ebaf774f2cb8f8600eb0
RyanVillenaUCR <rvill028@ucr.edu>
2019-03-22 23:19:29 -0700

    Added global constants for RGB colors

--- application.cpp
+++ application.cpp
@@ -20,7 +20,10 @@
 using namespace std;
 enum { NONE, AMBIENT, DIFFUSE, SPECULAR, NUM_MODES };
 
-const vec3 YELLOW(255.0f, 255.0f, 0.0f);
+//const vec3 YELLOW(255.0f, 255.0f, 255.0f);
+const vec3 YELLOW(1.0f, 1.0f, 0.0f);
+const vec3 GRAY(0.5f, 0.5f, 0.5f);
+const vec3 RED(1.0f, 0.0f, 0.0f);
 
 void draw_grid(int dim);
 void draw_obj(obj *o, const gl_image_texture_map& textures);
